a46dc72bb6ef7ff251a2b4264f9aa765a59989a9
Afan Khan <mrkhanads@gmail.com>
2023-06-14 19:08:33 +0530

    Completed Responsiveness

--- script.js
+++ script.js
@@ -8,8 +8,25 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(
-  `header, .hero-section, .packages-plan, .about-us, .testimonial, .newsletter-signup, .footer`,
+  `.hero-section, .packages-plan, .about-us, .about-us-real, .testimonial, .newsletter-signup, .footer`,
   {
     delay: 50,
   }
 );
+
+// Mobile Navigation
+
+const navOpenBtn = document.querySelector(".nav-open-btn");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+const header = document.querySelector(".header");
+const main = document.querySelector("main");
+
+navOpenBtn.addEventListener("click", () => {
+  header.classList.add("nav-open");
+  main.classList.add("nav-open");
+});
+
+navCloseBtn.addEventListener("click", () => {
+  header.classList.remove("nav-open");
+  main.classList.remove("nav-open");
+});
